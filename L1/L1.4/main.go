f73f8cc62e355c730885e1abce2bd6bbe06f2c06
ovsyannikvas <s.ovsyannikova7@gmail.com>
2025-09-04 14:20:02 +0300

    L1.4

---
+++ L1/L1.4/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func worker(ctx context.Context, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case i := <-ch:
+			fmt.Println(i)
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <num_workers>")
+		return
+	}
+
+	numWorkers, err := strconv.Atoi(os.Args[1])
+	if err != nil || numWorkers <= 0 {
+		fmt.Println("Invalid number of workers")
+		return
+	}
+
+	ch := make(chan int)
+
+	wg := sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// Главная пишущая горутина
+	go func(ctx context.Context) {
+		i := 0
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case ch <- i:
+				i++
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}(ctx)
+
+	wg.Add(numWorkers)
+	for range numWorkers {
+		go worker(ctx, ch, &wg)
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	cancel()
+	close(ch)
+	wg.Wait()
+}
